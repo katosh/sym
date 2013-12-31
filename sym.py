f8a12b54641d5a00e12d59d098ce9150643e70ec
Dominik Otto <dominik.otto@gmail.com>
2013-12-31 20:25:01 +0100

    more comments

--- sym.py
+++ sym.py
@@ -1,12 +1,11 @@
 import bpy
 import code
 import sys
-sys.path.append(r'.')
+sys.path.append(r'.')   # add script path to system pathes to find the other modules
 import globals as g
 
-
 """
-### Show the mesh in 3-d view
+### Show the used mesh in 3-d view
 mesh = bpy.data.meshes.new("Buttefly")
 g.bm.to_mesh(mesh)
 ob_new = bpy.data.objects.new("Butterfly", mesh)     
@@ -21,9 +20,9 @@ tr.mktransfs()  # make/fill transformation space
 tr.plotr()      # plotting the transformation space for refletions
 
 import clustering as cl
-cl.mkrgrid()
-# cl.showhemisphere()
-cl.rmeanshift()
-cl.showrlayer(0)
+cl.mkrgrid()        # make grid for reflectation space
+# cl.showhemisphere()   # show one offset layer of the grid
+cl.rmeanshift()     # calculate the density of transformations for each grid point
+cl.showrlayer(0)    # show one offset layer with hight (or distance to (0,0,0)) representig density
 
 import verification
