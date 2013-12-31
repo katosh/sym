d1def9f27523383914799049a14ea57d49095742
Dominik Otto <dominik.otto@gmail.com>
2013-12-31 19:48:18 +0100

    further modularization

--- globals.py
+++ globals.py
@@ -2,8 +2,9 @@ import bpy
 import bmesh
 bmesh.types.BMesh.free
 
-### global variables
+""" GLOBAL VARIABLES """
 
+### for geometry
 bm=bmesh.new()  # THE GEOMETRY
 scene = bpy.context.scene   # for visualisation
 
@@ -13,17 +14,19 @@ bpy.ops.mesh.primitive_monkey_add()
 bpy.ops.object.delete()
 bm.from_mesh(bpy.data.meshes["Suzanne"]) # get mesh data from model
 
-
+### for sigature space
 pc = 1.5	    # Pruning limit for curvature
 plimit = 0.1	# limit of relative curvature difference for pairing
-mrad = 0.08	    # relative radius for mean shift (1 is the whole space)
-mres = 50	    # resolution for Mean Shift
-
-
 sigs = []       # representation of signature space Omega
 pruned = []     # Point sorted out by pruning
 laenge = 0      # length of signature list
 
+### for transformation space
 transfs = []    # transformation space
 ntrans  = 0     # number of transformations
 moff    = 0     # maximal reflection plane offset
+sfaces  = []    # hemisphere faces for visualization
+
+### for clustering
+mrad = 0.08	    # relative radius for mean shift (1 is the whole space)
+mres = 50	    # resolution for Mean Shift
