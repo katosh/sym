6b6c3e83116242255d6192fe1cdcdc5422edbe15
Dominik Otto <dominik.otto@gmail.com>
2014-01-11 23:52:19 +0100

    Show status for Signature process that is curvature calculation and pruning

--- signatures.py
+++ signatures.py
@@ -1,6 +1,6 @@
 """ filling the signature space """
 
-import random
+import math
 
 class Signature:
     """ holds a point together with its signature """
@@ -15,10 +15,24 @@ class Signature:
 def mksigs(verts, maxverts=50):
     """ fill the signature space
         verts: a list of vertices"""
+
+    """ to show the status of the process """
+    steps = len(verts) # number of steps
+    step = 0 # current step
+    waitsteps = math.ceil(steps/1000) # steps befor showing percentage
+    slssteps = 0 # steps since last showing of percentage
+
     sigstemp = []
     for vert in verts:
         sig = Signature(vert)
         sigstemp.append(sig)
         sigstemp.sort(key=lambda x: x.curv, reverse=False) # sort by curvature
         sigs = sigstemp[0:maxverts]
+
+        """ showing process status """
+        slssteps += 1
+        if slssteps > waitsteps:
+            step += slssteps
+            print(' process at',math.floor(1000*step/steps)/10,'%', end='\r')
+            slssteps = 0
     return sigs
