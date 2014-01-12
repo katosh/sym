6aaf9a2b4fd43901fb557451389ed908cf91b06c
Dominik Otto <dominik.otto@gmail.com>
2014-01-12 17:14:15 +0100

    reduced bandwidth

--- meanshift.py
+++ meanshift.py
@@ -9,7 +9,7 @@ def k(delta,bandwidth):
 
 def cluster(gamma,
         steps=100,
-        bandwidth=0.02,
+        bandwidth=0.01,
         densitythreshold=5,
         offset_threshold=0.0001,
         cluster_resolution=0.01,
